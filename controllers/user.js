3d5288900fbe993e53fca32e5d7914272de6f33b
Dima Murugov <dimamurugov@gmail.com>
2021-01-23 21:52:29 +0300

    sameSite false

--- controllers/user.js
+++ controllers/user.js
@@ -18,7 +18,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: true,
+        sameSite: false,
       });
       res.status(200).send({
         name: user.name,
